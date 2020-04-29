704f85d3f7549dd9c6a4a16428212bab8ad19a8b
Attila Nacsa <Attila.Nacsa@etu.unige.ch>
2020-04-30 01:09:16 +0200

    cleanup

---
+++ Assets/Scripts/teleport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+    public GameObject objto;
+    public Transform objloc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Respawn")
+        {
+            this.transform.position = objloc.transform.position;
+        }
+    }
+}
