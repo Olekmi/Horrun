2027152a4a88099f3975ae2e8da293f98c53bf4c
Attila Nacsa <Attila.Nacsa@etu.unige.ch>
2020-05-01 01:54:33 +0200

    adding mesh grid generator

---
+++ Assets/MeshTerrain/Scripts/TerrainGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    public int width = 200;
+    public int length = 512;
+    public int height = 521;
+    
+    public float offsetY = 0;
+
+    public float offsetXPerlin = 0;
+    public int scale = 20;
+
+    private Terrain terrain;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetXPerlin = Random.Range(0f, 9999f);
+        offsetY = transform.position.y;
+
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);   
+    }
+
+    TerrainData GenerateTerrain(TerrainData terrainData) 
+    {
+        terrainData.size = new Vector3(width, height, length);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+
+        return terrainData;
+    }
+
+    float[,] GenerateHeights ()
+    {
+        float[,] heights = new float[width, length];
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < length; ++y) {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight(int x, int y) 
+    {
+        float xCoord = (float)x / width * scale + offsetXPerlin;
+        float yCoord = (float)y / length * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour
+{
+    // grid size
+    public int xSize = 50;
+    public int zSize = 50;
+    public Gradient gradient;
+    public float maxTerrainHeight = 5f;
+    public float heightModifier = 5f;
+
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+    Color[] colours;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //CreateShape();
+        //UpdateMesh();
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    void CreateShape()
+    {
+        // generate vertices
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        float heightLimiter = 0;
+        float xSizeHalf = (float)xSize/2;
+        for (int i = 0, z = 0; z <= zSize; ++z)
+        {
+            for (int x = 0; x <= xSize; ++x)
+            {
+                heightLimiter = Mathf.Max((Mathf.Abs(x - xSizeHalf) - 10)/(xSizeHalf - 10), 0);
+                float y = (Mathf.PerlinNoise(x * .3f, z * .3f) * maxTerrainHeight + heightModifier) * heightLimiter;
+
+                vertices[i] = new Vector3(x, y, z);
+                ++i;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        // draw quads between vertices
+        for (int vert = 0, tris = 0, z = 0; z < zSize; ++z)
+        {
+            for (int x = 0; x < xSize; ++x)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                ++vert;
+                tris += 6;
+            }
+            ++vert;
+        }
+
+        // colour the vertices based on height
+        colours = new Color[vertices.Length];
+
+        for (int i = 0, z = 0; z <= zSize; ++z)
+        {
+            for (int x = 0; x <= xSize; ++x)
+            {
+                float height = Mathf.InverseLerp(0, maxTerrainHeight + heightModifier, vertices[i].y);
+                colours[i] = gradient.Evaluate(height);
+                ++i;
+            }
+        }
+    }
+
+    void UpdateMesh()
+    {
+        // delete old data
+        mesh.Clear();
+
+        // load in generated data
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.colors = colours;
+
+        // recalculate lighting
+        mesh.RecalculateNormals();
+
+        // collision
+        mesh.RecalculateBounds();
+        MeshCollider meshCollider = gameObject.GetComponent<MeshCollider>();
+        meshCollider.sharedMesh = mesh;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+
+        for (int i = 0; i < vertices.Length; ++i)
+        {
+            Gizmos.DrawSphere(vertices[i] + transform.position, .1f);
+        }
+    }
+}
