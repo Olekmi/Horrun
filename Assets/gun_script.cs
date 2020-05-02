b3bd61a648663260265185a82b5814e1724dbaa3
Olekmi <michonskialeksander@gmail.com>
2020-05-02 13:42:15 +0200

    Hit & Camera preview
    Laser hit working - objects disappear

--- Assets/gun_script.cs
+++ Assets/gun_script.cs
@@ -6,7 +6,8 @@ public class gun_script : MonoBehaviour
     public float range = 100f;
     public Camera fpsCam;
     // Start is called before the first frame update
-    void Start()
+
+    void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
@@ -21,6 +22,12 @@ public class gun_script : MonoBehaviour
         if (Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
         }
     }
 }
